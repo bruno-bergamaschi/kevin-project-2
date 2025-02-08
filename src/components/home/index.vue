2cb76667a17ea91e65daae0905e2df938052247a
Bruno Bergamaschi <brunobergamaschi@live.com>
2025-02-07 22:08:16 -0300

    initial commit

---
+++ src/components/home/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+    <v-row no-gutters class="line">
+      <v-col cols="6" class="line__column--red">Kevin</v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script type="text/javascript" src="./script.js" />
+
+<style scoped lang="scss" src="./style.scss" />
